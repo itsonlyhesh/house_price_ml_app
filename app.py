ed82e8ebe25003b56a5f3e27250220d02c41b3a0
Heshwanth Reddy <bheshwanth.reddy720@gmail.com>
2026-08-06 11:51:31 +0530

    updated

--- app.py
+++ app.py
@@ -1,24 +1,27 @@
-from flask import Flask,request,render_template
+from flask import Flask, request, render_template
 import joblib
 import numpy as np
-app=Flask(__name__)
+import os
 
-model=joblib.load("model.pkl")
+app = Flask(__name__)
+
+model = joblib.load("model.pkl")
+
 @app.route("/")
 def home():
     return render_template("main.html")
 
-@app.route("/predict",methods=["POST"])
+@app.route("/predict", methods=["POST"])
 def predict():
-    area=float(request.form["area"])
-    bedrooms=int(request.form["bedrooms"])
-    age=int(request.form["age"])
-    
-    features=np.array([[area,bedrooms,age]])
-    prediction=model.predict(features)[0]
-    return render_template("main.html",prediction_text=f"Estimated price:₹{prediction:,.0f}")
-import os
-if __name__=="__main__":
-    port=int(os.environ.get("PORT",5000))
-    app.run(host="0.0.0.0",port=port)
-    
+    area = float(request.form["area"])
+    bedrooms = int(request.form["bedrooms"])
+    age = int(request.form["age"])
+
+    features = np.array([[area, bedrooms, age]])
+    prediction = model.predict(features)[0]
+
+    return render_template("main.html", prediction_text=f"Estimated price: ${prediction:,.0f}")
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
